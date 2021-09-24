1ebdc1d97899ef802df39b336af872d36ae5c65f
Lessalc <lucianoclessa@gmail.com>
2021-09-23 23:19:28 -0300

    Primeiro commit - Criar arquivo index.html e main.js
    Nesse programa de treinamento estamos treinando Javascript
    Nesse primeiro commit já criamos um arquivo .js que irá apresentar
    algumas coisas no console.
    Também temos uma alerta comentada que foi usada para aprendizagem

---
+++ javascript/main.js
@@ -0,0 +1,12 @@
+var nome = "Luciano Lessa";
+
+//alert(`Hello World - ${nome}`);
+
+var idade = 31;
+var numAleatorio = 20;
+var frase = "Luciano Lessa é o melhor desenvolvedor, em aprendizagem, do mundo";
+
+
+console.log(nome);
+console.log(idade + numAleatorio);
+console.log(frase.replace("do mundo", "das galáxias kkkkkk").toUpperCase());
